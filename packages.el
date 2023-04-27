eaebe13b3965db5761b841423c82e63ce3e9e0a6
Sebastian Aburto <seba.aburto@gmail.com>
2023-04-27 14:50:54 -0400

    feat: add java-snippets and mvn-test-mode

--- packages.el
+++ packages.el
@@ -42,7 +42,37 @@
 
 (defconst saburto-java-packages
   '((java-ts-mode :location built-in)
-    (lsp-java :requires lsp-mode)))
+    (lsp-java :requires lsp-mode)
+    (java-snippets
+      :requires yasnippet
+      :location (recipe :fetcher github
+                        :files ("*.el" "snippets")
+                                     :repo "saburto/yasnippet-java-mode"))
+    maven-test-mode
+
+    ))
+
+
+(defun saburto-java/init-maven-test-mode ()
+  (use-package maven-test-mode
+    :defer t
+    :init
+    (when (configuration-layer/package-used-p 'java-ts-mode)
+      (add-hook 'java-ts-mode-hook 'maven-test-mode)
+      (spacemacs/declare-prefix-for-mode 'java-ts-mode "mm" "maven")
+      (spacemacs/declare-prefix-for-mode 'java-ts-mode "mtg" "goto to tests")
+      (spacemacs/declare-prefix-for-mode 'java-ts-mode "mmt" "tests"))
+    :config
+    (progn
+      (spacemacs|hide-lighter maven-test-mode)
+      (spacemacs/set-leader-keys-for-minor-mode 'maven-test-mode
+        "tga"    'maven-test-toggle-between-test-and-class
+        "tgA"    'maven-test-toggle-between-test-and-class-other-window
+        "mta"    'maven-test-all
+        "mt C-a" 'maven-test-clean-test-all
+        "mtb"    'maven-test-file
+        "mti"    'maven-test-install
+        "mtt"    'maven-test-method))))
 
 (defun saburto-java/init-java-ts-mode ()
   (use-package java-ts-mode
@@ -50,10 +80,36 @@
     :mode (("\\.java\\'" . java-ts-mode))
     :hook ((java-ts-mode . (lambda () (setq c-basic-offset 4 tab-width 4))))))
 
-(defun saburto-java/init-lsp-java()
+(defun saburto-java/init-lsp-java ()
 
   (use-package lsp-java
     :init
     (setq lsp-java-jdt-download-url "https://download.eclipse.org/jdtls/snapshots/jdt-language-server-latest.tar.gz")
     (setq lsp-java-vmargs '("-noverify" "-Xmx4G"  "-XX:+UseG1GC" "-XX:+UseStringDeduplication" "-javaagent:/home/saburto/.m2/repository/org/projectlombok/lombok/1.18.24/lombok-1.18.24.jar" ))
-    :hook ((java-ts-mode . lsp-deferred))))
+    :hook ((java-ts-mode . lsp-deferred))
+    :config
+    (progn
+      (dolist (prefix '(("mc" . "compile/create")
+                        ("mgk" . "type hierarchy")
+                        ("mt" . "test")))
+        (spacemacs/declare-prefix-for-mode
+          'java-ts-mode (car prefix) (cdr prefix)))
+
+      (spacemacs/set-leader-keys-for-major-mode 'java-ts-mode
+        "wu"  'lsp-java-update-project-configuration
+
+        "ro" 'lsp-java-organize-imports
+
+        "cc"  'lsp-java-build-project
+
+        "gkk" 'lsp-java-type-hierarchy
+        "gku" 'spacemacs/lsp-java-super-type
+        "gks" 'spacemacs/lsp-java-sub-type
+        )
+
+
+    )))
+
+(defun saburto-java/init-java-snippets ()
+  (use-package java-snippets
+    :defer t))
